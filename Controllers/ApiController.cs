275ae12fdb8f4d5e770f79121823b3a2a43b99ab
陳茜 <aass600717@gmail.com>
2024-05-22 22:46:09 +0800

    5

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -18,43 +18,42 @@ namespace MSIT158Site.Controllers
             _hostEnvironment = hostEnvironment;
         }
 
-
-
+        // 首頁
         public IActionResult Index()
         {
             System.Threading.Thread.Sleep(10000);
             return Content("世界, 您好!!", "text/html", System.Text.Encoding.UTF8);
         }
 
-        //讀出不會重複的城市名
+        // 讀出不會重複的城市名
         public IActionResult Cities()
         {
             var cities = _context.Addresses.Select(a => a.City).Distinct();
             return Json(cities);
         }
 
-        //根據城市名讀出不會重複的鄉鎮區
+        // 根據城市名讀出不會重複的鄉鎮區
         public IActionResult Districts(string city)
         {
             var districts = _context.Addresses.Where(a => a.City == city).Select(a => a.SiteId).Distinct();
             return Json(districts);
         }
-        //根據鄉鎮區讀出路名
+
+        // 根據鄉鎮區讀出路名
         public IActionResult Roads(string districts)
         {
             var roads = _context.Addresses.Where(a => a.SiteId == districts).Select(a => a.Road);
             return Json(roads);
         }
 
-        //檢查帳號是否存在
+        // 檢查帳號是否存在
         public IActionResult CheckAccount(string name)
         {
             var member = _context.Members.Any(m => m.Name == name);
-
             return Content(member.ToString(), "text/plain", System.Text.Encoding.UTF8);
         }
 
-
+        // 讀取會員頭像
         public IActionResult Avatar(int id = 1)
         {
             Member? member = _context.Members.Find(id);
@@ -65,12 +64,11 @@ namespace MSIT158Site.Controllers
                 {
                     return File(img, "image/jpeg");
                 }
-
             }
             return NotFound();
         }
 
-        //public IActionResult Register(string userName, string email, int age = 20)
+        // 註冊新會員並上傳頭像
         public IActionResult Register(Member member, IFormFile avatar)
         {
             if (string.IsNullOrEmpty(member.Name))
@@ -78,18 +76,7 @@ namespace MSIT158Site.Controllers
                 member.Name = "guest";
             }
 
-            //取得上傳檔案的資訊
-            //string info = $"{avatar.FileName} - {avatar.Length} - {avatar.ContentType}";
-            //string info = _hostEnvironment.ContentRootPath;
-
-            //檔案上傳寫進資料夾
-            //todo1 判斷檔案是否存在
-            //todo2 限制上傳檔案的大小跟類型 
-
-            //實際路徑
-            //string uploadPath = @"C:\Users\User\Documents\workspace\MSIT158Site\wwwroot\uploads\a.png";
-            //WebRootPath：C: \Users\User\Documents\workspace\MSIT158Site\wwwroot
-            //ContentRootPath：C:\Users\User\Documents\workspace\MSIT158Site
+            // 實際路徑
             string uploadPath = Path.Combine(_hostEnvironment.WebRootPath, "uploads", avatar.FileName);
             string info = uploadPath;
             using (var fileStream = new FileStream(uploadPath, FileMode.Create))
@@ -97,7 +84,7 @@ namespace MSIT158Site.Controllers
                 avatar.CopyTo(fileStream);
             }
 
-            //檔案上傳轉成二進位
+            // 檔案上傳轉成二進位
             byte[]? imgByte = null;
             using (var memoryStream = new MemoryStream())
             {
@@ -105,29 +92,28 @@ namespace MSIT158Site.Controllers
                 imgByte = memoryStream.ToArray();
             }
 
-            //寫進資料庫
+            // 寫進資料庫
             member.FileName = avatar.FileName;
             member.FileData = imgByte;
             _context.Members.Add(member);
             _context.SaveChanges();
 
             return Content(info, "text/plain", System.Text.Encoding.UTF8);
-            // return Content($"Hello {member.Name}，{member.Age} 歲了，電子郵件是 {member.Email}", "text/html", System.Text.Encoding.UTF8);
         }
 
         [HttpPost]
+        // 根據分類編號和關鍵字搜尋景點資料
         public IActionResult Spots([FromBody] SearchDTO searchDTO)
         {
-            //根據分類編號搜尋景點資料
             var spots = searchDTO.categoryId == 0 ? _context.SpotImagesSpots : _context.SpotImagesSpots.Where(s => s.CategoryId == searchDTO.categoryId);
 
-            //根據關鍵字搜尋景點資料(title、desc)
+            // 根據關鍵字搜尋景點資料(title、desc)
             if (!string.IsNullOrEmpty(searchDTO.keyword))
             {
                 spots = spots.Where(s => s.SpotTitle.Contains(searchDTO.keyword) || s.SpotDescription.Contains(searchDTO.keyword));
             }
 
-            //排序
+            // 排序
             switch (searchDTO.sortBy)
             {
                 case "spotTitle":
@@ -141,33 +127,26 @@ namespace MSIT158Site.Controllers
                     break;
             }
 
-
-
-
-
-            //總共有多少筆資料
+            // 總共有多少筆資料
             int totalCount = spots.Count();
-            //每頁要顯示幾筆資料
-            int pageSize = searchDTO.pageSize;   //searchDTO.pageSize ?? 9;
-            //目前第幾頁
+            // 每頁要顯示幾筆資料
+            int pageSize = searchDTO.pageSize;
+            // 目前第幾頁
             int page = searchDTO.page;
 
-            //計算總共有幾頁
+            // 計算總共有幾頁
             int totalPages = (int)Math.Ceiling((decimal)totalCount / pageSize);
 
-            //分頁
+            // 分頁
             spots = spots.Skip((page - 1) * pageSize).Take(pageSize);
 
-
-            //包裝要傳給client端的資料
+            // 包裝要傳給client端的資料
             SpotsPagingDTO spotsPaging = new SpotsPagingDTO();
             spotsPaging.TotalCount = totalCount;
             spotsPaging.TotalPages = totalPages;
             spotsPaging.SpotsResult = spots.ToList();
 
-
             return Json(spotsPaging);
         }
-
     }
 }
