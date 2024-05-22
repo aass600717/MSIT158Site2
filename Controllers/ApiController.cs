6fc45a8314de9c2637aac0a7c81d064148dde327
Chen <aass600717@gmail.com>
2024-05-22 17:04:38 +0800

    05222

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -2,6 +2,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Hosting;
 using MSIT158Site.Models;
+using MSIT158Site.Models.DTO;
 using Newtonsoft.Json;
 
 namespace MSIT158Site.Controllers
@@ -81,7 +82,10 @@ namespace MSIT158Site.Controllers
             //string info = $"{avatar.FileName} - {avatar.Length} - {avatar.ContentType}";
             //string info = _hostEnvironment.ContentRootPath;
 
-            //檔案上傳
+            //檔案上傳寫進資料夾
+            //todo1 判斷檔案是否存在
+            //todo2 限制上傳檔案的大小跟類型 
+
             //實際路徑
             //string uploadPath = @"C:\Users\User\Documents\workspace\MSIT158Site\wwwroot\uploads\a.png";
             //WebRootPath：C: \Users\User\Documents\workspace\MSIT158Site\wwwroot
@@ -93,12 +97,77 @@ namespace MSIT158Site.Controllers
                 avatar.CopyTo(fileStream);
             }
 
-            return Content(info, "text/plain", System.Text.Encoding.UTF8);
-            //寫進資料庫
-            //_context.Members.Add(member);
-            //_context.SaveChanges();
+            //檔案上傳轉成二進位
+            byte[]? imgByte = null;
+            using (var memoryStream = new MemoryStream())
+            {
+                avatar.CopyTo(memoryStream);
+                imgByte = memoryStream.ToArray();
+            }
 
+            //寫進資料庫
+            member.FileName = avatar.FileName;
+            member.FileData = imgByte;
+            _context.Members.Add(member);
+            _context.SaveChanges();
+
+            return Content(info, "text/plain", System.Text.Encoding.UTF8);
             // return Content($"Hello {member.Name}，{member.Age} 歲了，電子郵件是 {member.Email}", "text/html", System.Text.Encoding.UTF8);
         }
+
+        [HttpPost]
+        public IActionResult Spots([FromBody] SearchDTO searchDTO)
+        {
+            //根據分類編號搜尋景點資料
+            var spots = searchDTO.categoryId == 0 ? _context.SpotImagesSpots : _context.SpotImagesSpots.Where(s => s.CategoryId == searchDTO.categoryId);
+
+            //根據關鍵字搜尋景點資料(title、desc)
+            if (!string.IsNullOrEmpty(searchDTO.keyword))
+            {
+                spots = spots.Where(s => s.SpotTitle.Contains(searchDTO.keyword) || s.SpotDescription.Contains(searchDTO.keyword));
+            }
+
+            //排序
+            switch (searchDTO.sortBy)
+            {
+                case "spotTitle":
+                    spots = searchDTO.sortType == "asc" ? spots.OrderBy(s => s.SpotTitle) : spots.OrderByDescending(s => s.SpotTitle);
+                    break;
+                case "categoryId":
+                    spots = searchDTO.sortType == "asc" ? spots.OrderBy(s => s.CategoryId) : spots.OrderByDescending(s => s.CategoryId);
+                    break;
+                default:
+                    spots = searchDTO.sortType == "asc" ? spots.OrderBy(s => s.SpotId) : spots.OrderByDescending(s => s.SpotId);
+                    break;
+            }
+
+
+
+
+
+            //總共有多少筆資料
+            int totalCount = spots.Count();
+            //每頁要顯示幾筆資料
+            int pageSize = searchDTO.pageSize;   //searchDTO.pageSize ?? 9;
+            //目前第幾頁
+            int page = searchDTO.page;
+
+            //計算總共有幾頁
+            int totalPages = (int)Math.Ceiling((decimal)totalCount / pageSize);
+
+            //分頁
+            spots = spots.Skip((page - 1) * pageSize).Take(pageSize);
+
+
+            //包裝要傳給client端的資料
+            SpotsPagingDTO spotsPaging = new SpotsPagingDTO();
+            spotsPaging.TotalCount = totalCount;
+            spotsPaging.TotalPages = totalPages;
+            spotsPaging.SpotsResult = spots.ToList();
+
+
+            return Json(spotsPaging);
+        }
+
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -47,5 +47,10 @@ namespace MSIT158Site.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Spots()
+        {
+            return View();
+        }
     }
 }
